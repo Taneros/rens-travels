0b6a5fee1fdb727a02bc68d7bfe1935169b8e289
Taner <fricentaurus@gmail.com>
2020-10-13 11:26:15 +0300

    Setup the file structure for css, html, js

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp')
+const watch = require('gulp-watch')
+const browserSync = require('browser-sync').create()
+
+gulp.task('watch', async function () {
+    watch('./app/assets/styles/**/*.css', gulp.series('styles', 'cssInject'))
+    watch('./app/index.html', () => browserSync.reload())
+    // watch('./app/index.html', gulp.series('html'))
+    browserSync.init({
+        server: {
+            baseDir: "app",
+            notify: false
+        }
+    })
+})
+
+// gulp.task('html', function () {
+//     browserSync.reload()
+// })
+
+gulp.task('cssInject', async function () {
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream())
+})
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+require('./gulp/tasks/watch.js')
+require('./gulp/tasks/styles.js')
+
+
+// moved to watch.js
+// gulp.task('watch', async function () {
+//     watch('./app/assets/styles/**/*.css', gulp.series('styles', 'cssInject'))
+//     watch('./app/index.html', gulp.series('html'))
+//     browserSync.init({
+//         server: {
+//             baseDir: "app",
+//             notify: false
+//         }
+//     })
+// })
+
+// moved to watch.js
+// gulp.task('html', function () {
+//     browserSync.reload()
+// })
+
+// moved to styles.js
+// gulp.task('styles', async function () {
+//     return gulp.src('./app/assets/styles/styles.css')
+//         .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
+//         .pipe(gulp.dest('./app/temp/styles'))
+// })
+
+// moved to watch.js
+// gulp.task('cssInject', async function () {
+//     return gulp.src('./app/temp/styles/styles.css')
+//         .pipe(browserSync.stream())
+// })
