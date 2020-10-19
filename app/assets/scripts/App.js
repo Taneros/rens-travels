83c370088e0b6b88a491854bf30f13ff809eeae0
Taner <fricentaurus@gmail.com>
2020-10-19 12:57:48 +0300

    Added on-scroll reveal function. Extra zoom-out effect.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
 import MobileMenu from './modules/MobileMenu.js'
+import RevealOnScroll from './modules/RevealOnScroll.js'
 
 let mobileMenu = new MobileMenu()
+// let revealOnScroll = new RevealOnScroll()
+new RevealOnScroll(document.querySelectorAll('.feature-item'), '85%')
+new RevealOnScroll(document.querySelectorAll('.testimonial'), '60%')
