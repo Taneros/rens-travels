adbb426ac65e304a4f9d35861a7185f514fcc944
Taner <fricentaurus@gmail.com>
2020-10-20 13:51:49 +0300

    Completed modal lightbox overlay

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -10,6 +10,22 @@ class StickyHeader {
         this.createPageSectionWaypoints()
     }
 
+    //Events //TODO work on these events to add return
+    // events() {
+    //     //click the open modal button
+    //     this.headerLinks.forEach((elem) => {
+    //         elem.addEventListener('click', () => {
+    // event.preventDefault()            
+    // smoothScroll(document.getElementById('our-beginning'))
+    //         })
+    //     })
+    //     //click the x close modal button on modal overlay
+    //     this.closeModalButton.addEventListener('click', () => {
+    //         this.closeModal
+    //     })
+    //     //pushes escape key on the keyboard
+    // }
+
     // Methods
     createHeaderWaypoint() {
         new Waypoint({
@@ -28,12 +44,6 @@ class StickyHeader {
         this.pageSections.forEach(elem => {
             new Waypoint({
                 element: elem, //div element
-                // no if statement handler
-                // handler: () => {
-                //     let matchingHeaderLink = elem.getAttribute("data-matching-link") // get the 'data-matching-link' Attribute value stored in div
-                //     this.headerLinks.forEach(elem => elem.classList.remove("is-current-link")) // remove is-current-link before highlighting next section
-                //     document.querySelector(matchingHeaderLink).classList.add("is-current-link")
-                // }
                 handler: (direction) => {
                     if (direction == "down") {
                         let matchingHeaderLink = elem.getAttribute("data-matching-link") // get the 'data-matching-link' Attribute value stored in div
@@ -56,7 +66,6 @@ class StickyHeader {
             })
         })
     }
-
 }
 
 export default StickyHeader
